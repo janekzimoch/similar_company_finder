58d89cf56d11ab360190d05a52234015a2510816
Janek Zimoch <janek@Janeks-MacBook-Pro.local>
2022-11-19 16:07:39 +0000

    created UI interface for the website. Now I need to build a database adn connect it to the interface logic

--- submit_url/views.py
+++ submit_url/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "submit_url/index.html")
+    output_companies = {1: {'name': 'Microsoft'},
+                        2: {'name': 'Facebook'},
+                        3: {'name': 'Amazon'}}
+    if request.method == "POST":
+        print(request.POST['input_field'])
+        return render(request, "submit_url/index.html", {
+            "output_companies": output_companies,
+        })
+    else:
+        return render(request, "submit_url/index.html")
